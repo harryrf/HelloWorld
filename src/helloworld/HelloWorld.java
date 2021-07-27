e9a771d87b9de71086b20aeb95f530f2ad52867e
Harry Fahringer III <harryrf@users.noreply.github.com>
2021-07-27 15:59:27 -0400

    Initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ *
+ * @author Harry Fahringer III
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
